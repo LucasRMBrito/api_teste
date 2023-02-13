cae18afc9d23c0f76eb89f5366210ee9fd1291f0
LucasBrito <lucasroger.mb@hotmail.com>
2023-02-13 02:03:47 -0300

    teste cors 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const cors = require('cors');
+const cors = require('cors')
 
 //forma de ler JSON / middlewares
 app.use(
