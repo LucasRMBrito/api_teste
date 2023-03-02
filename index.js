7982583059023620a58855b71f64ade9ad241867
LucasBrito <lucasroger.mb@hotmail.com>
2023-03-02 10:24:02 -0300

    mudança de porta para 5000

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.3xe14db.mongodb.net/bancodaapi?retryWrites=true&w=majority`)
 .then(() => {
     console.log('Conectamos ao MongoDB!!!')
-    app.listen(3000)
+    app.listen(5000)
 })
 .catch((err) => console.log(err))
 
